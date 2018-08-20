0d0ebe08b8f41dfa79cc5e7ce5fd438ed35d1ef8
Raghavan R <raghavan.r@capgemini.com>
2018-08-21 09:14:24 +1000

    adding names

--- beanstalk.tf
+++ beanstalk.tf
@@ -1,6 +1,6 @@
 resource "aws_elastic_beanstalk_application" "beanstalkapp" {
   count = ${var.create_vpc ? 1 : 0}
-  name        = "${var.env}${var.appname}"
+  name        = "${var.env}${var.appname}-0${count.index + 1}"
   description = "${var.description}"
 
   appversion_lifecycle {
@@ -12,7 +12,7 @@ resource "aws_elastic_beanstalk_application" "beanstalkapp" {
 
 resource "aws_elastic_beanstalk_environment" "beanstalkenv" {
   count = ${var.create_vpc ? 1 : 0}
-  name                = "${var.appname}-env"
+  name                = "${var.appname}-env0${count.index + 1}"
   application         = "${aws_elastic_beanstalk_application.beanstalkapp.name}"
   cname_prefix = "${var.appname}"
   tier = "${var.tier}"
